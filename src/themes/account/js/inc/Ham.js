5fd63b16613996997f0f07ada1e5b64ec5bd8e11
Studio Champ Gauche <comptes@champgauche.studio>
2025-06-01 19:10:56 -0400

    Update Ham.js

--- src/themes/account/js/inc/Ham.js
+++ src/themes/account/js/inc/Ham.js
@@ -6,118 +6,61 @@ import { CustomEase } from 'gsap/CustomEase';
 const Ham = {
     init: () => {
 
-        gsap.registerPlugin(ScrollTrigger, CustomEase);
+        const headerElement = document.querySelector('.side');
+        const hamElement = headerElement?.querySelector('.ham-menu');
 
-        const headerElement = document.querySelector('header');
+        if(!hamElement) return;
 
-        const logoElement = headerElement.querySelector('.logo');
+        let openAnim = gsap.timeline();
 
+        openAnim
+        .to(headerElement, .4, {
+            height: '100vh'
+        })
+        .paused(true);
 
-        const btn = headerElement.querySelector('.btn');
-        const btnSpans = btn.querySelectorAll('span');
+        const handleClick = () => {
 
+            hamElement.classList.toggle('active');
 
-        const headerAnimation = gsap.timeline();
+            if(hamElement.classList.contains('active')){
 
-        let lastDirection = -1;
+                window.gscroll.paused(true);
 
+                openAnim.play();
+                openAnim.reversed(false);
 
-        headerAnimation
-        .to(headerElement.querySelector('.plywood'), .4, {
-            y: '-100%',
-            ease: 'none'
-        });
+            } else {
 
-        headerAnimation.paused(true);
+                window.gscroll.paused(false);
 
-        
-        ScrollTrigger.create({
-            id: 'header',
-            trigger: '#pageContent',
-            start: 'top top',
-            end: 'bottom bottom',
-            scrub: true,
-            onUpdate: (self) => {
+                openAnim.play();
+                openAnim.reversed(true);
 
+            }
 
-                if(lastDirection === self.direction) return;
+        }
 
-                lastDirection = self.direction;
+        hamElement.addEventListener('click', handleClick);
 
-                if(lastDirection === 1){
+        return {
 
-                    headerAnimation.play();
-                    headerAnimation.reversed(false);
+            kill: () => {
 
-                } else {
+                hamElement.removeEventListener('click', handleClick);
 
-                    headerAnimation.play();
-                    headerAnimation.reversed(true);
+                if(openAnim){
+
+                    openAnim.kill();
+                    openAnim = null;
 
                 }
 
             }
-        });
 
-
-        let canEnter = true;
-        const logoAnim = gsap.timeline();
-
-        logoAnim
-        .to(logoElement, .2, {
-            scale: .9
-        })
-        .paused(true);
-
-
-        logoElement.addEventListener('mouseenter', () => {
-
-            logoAnim.play();
-            logoAnim.reversed(false);
-
-        });
-
-
-        logoElement.addEventListener('mouseleave', () => {
-
-            logoAnim.play();
-            logoAnim.reversed(true);
-
-        });
-
-
-
-
-        const tl = gsap.timeline();
-
-        tl
-        .to(btnSpans[1], .2, {
-            y: '+=20px',
-            opacity: 0
-        })
-        .to(btnSpans[0], .2, {
-            y: '+=20px',
-            opacity: 1
-        }, 0)
-        .paused(true);
-
-
-        btn.addEventListener('mouseenter', () => {
-
-            tl.play();
-            tl.reversed(false);
-
-        });
-
-
-        btn.addEventListener('mouseleave', () => {
-
-            tl.play();
-            tl.reversed(true);
-            
-        });
-
+        }
     }
 }
 
-Ham.init();
+
+export default Ham;
