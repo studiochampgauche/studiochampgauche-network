568a1fe6da752280b447ca372a0695b8a851ada5
Studio Champ Gauche <comptes@champgauche.studio>
2025-06-05 00:00:19 -0400

    Add hubspot webhook
    When deal is won, add the contact associate to the deal as Stripe client et WordPress user.

--- src/themes/account/template/inc/endpoint.php
+++ src/themes/account/template/inc/endpoint.php
@@ -82,6 +82,237 @@ add_action('rest_api_init', function(){
         'permission_callback' => '__return_true'
     ]);
 
+
+    register_rest_route('siterapide/v1', 'dealwin', [
+        'methods' => 'POST',
+        'callback' => function(){
+
+            $secret = getenv('HUBSPOT_CLIENT_SECRET');
+            $payload = @file_get_contents('php://input');
+            $signature = $_SERVER['HTTP_X_HUBSPOT_SIGNATURE_V3'];
+            $timestamp = $_SERVER['HTTP_X_HUBSPOT_REQUEST_TIMESTAMP'];
+
+            // Calcul de la signature attendue
+            $computed_signature = base64_encode(hash_hmac('sha256', 'POSThttps://moncompte.siterapide.ca/wp-json/siterapide/v1/dealwin'.$payload.$timestamp, $secret, true));
+
+            // Vérification sécurisée
+            if ($computed_signature !== $signature) {
+
+                http_response_code(401);
+                echo 'Signature invalide';
+
+                exit;
+
+            }
+
+            
+            $data = json_decode($payload, true);
+
+            foreach ($data as $event) {
+                if (
+                    $event['subscriptionType'] === 'deal.propertyChange'
+
+                    &&
+
+                    $event['propertyName'] === 'dealstage'
+
+                    &&
+
+                    $event['propertyValue'] === 'closedwon'
+                ) {
+
+                    require_once 'composer/vendor/autoload.php';
+
+
+                    $dealId = $event['objectId'];
+                    $customer = null;
+
+
+                    $hubspot = \HubSpot\Factory::createWithAccessToken(getenv('HUBSPOT_TOKEN'));
+
+                    $publicFetchAssociationsBatchRequest1 = new \HubSpot\Client\Crm\Associations\V4\Model\PublicFetchAssociationsBatchRequest([
+                        'id' => $dealId
+                    ]);
+
+                    $batchInputPublicFetchAssociationsBatchRequest = new \HubSpot\Client\Crm\Associations\V4\Model\BatchInputPublicFetchAssociationsBatchRequest([
+                        'inputs' => [$publicFetchAssociationsBatchRequest1],
+                    ]);
+
+                    try {
+
+                        $assoResponse = json_decode($hubspot->crm()->associations()->v4()->batchApi()->getPage('deals', 'contacts', $batchInputPublicFetchAssociationsBatchRequest), true);
+                        $contactID = $assoResponse['results'][0]['to'][0]['toObjectId'];
+
+                        $contactResponse = json_decode($hubspot->crm()->contacts()->basicApi()->getById($contactID, false, ['firstname', 'lastname', 'email', 'phone', 'company']), true);
+
+                        $contactFirstname = $contactResponse['propertiesWithHistory']['firstname'][0]['value'];
+                        $contactLastname = $contactResponse['propertiesWithHistory']['lastname'][0]['value'];
+                        $contactEmail = $contactResponse['propertiesWithHistory']['email'][0]['value'];
+                        $contactPhone = $contactResponse['propertiesWithHistory']['phone'][0]['value'];
+                        $contactCompany = $contactResponse['propertiesWithHistory']['company'][0]['value'];
+
+                    } catch (\HubSpot\Client\Crm\Associations\V4\ApiException $e) {
+
+                        echo $e->getMessage();
+
+                        exit;
+
+                    } catch (\HubSpot\Client\Crm\Contacts\ApiException $e) {
+
+                        echo $e->getMessage();
+
+                        exit;
+
+                    }
+
+
+
+                    $userExists = get_user_by('email', $contactEmail);
+
+                    if($userExists){
+
+                        echo 'passed';
+
+                        http_response_code(200);
+
+                        exit;
+
+                    }
+
+
+                    try{
+
+                        $stripe = new \Stripe\StripeClient(STRIPE_SECRET_KEY);
+
+                        $customer = $stripe->customers->create([
+                            'name' => $contactFirstname . ' ' . $contactLastname,
+                            'email' => $contactEmail,
+                            'phone' => $contactPhone,
+                            'metadata' => ['company' => $contactCompany]
+                        ]);
+
+
+                        $fakePassword = hash_hmac('sha256', $contactEmail . ':' . time(), getenv('SECRET_SALT'));
+
+                        $wpUserID = wp_insert_user([
+                            'first_name' => $contactFirstname,
+                            'last_name' => $contactLastname,
+                            'user_login' => str_replace('cus_', '', $customer->id),
+                            'user_email' => $contactEmail,
+                            'user_pass' => $fakePassword,
+                            'role' => 'member'
+                        ]);
+
+                        update_field('field_683432a005d33', $customer->id, 'user_' . $wpUserID);
+                        update_field('field_683432b605d34', $phone, 'user_' . $wpUserID);
+                        update_field('field_683432d705d35', $company, 'user_' . $wpUserID);
+
+                    } catch(\Stripe\Exception\CardException $e) {
+
+                        if($customer){
+                            $stripe->customers->delete($customer->id, []);
+                        }
+
+                        echo 'error';
+
+                        exit;
+
+                    } catch (\Stripe\Exception\RateLimitException $e) {
+
+                        if($customer){
+                            $stripe->customers->delete($customer->id, []);
+                        }
+
+                        echo 'error';
+
+                        exit;
+
+                    } catch (\Stripe\Exception\InvalidRequestException $e) {
+
+                        if($customer){
+                            $stripe->customers->delete($customer->id, []);
+                        }
+
+                        echo 'error';
+
+                        exit;
+
+                    } catch (\Stripe\Exception\AuthenticationException $e) {
+
+                        if($customer){
+                            $stripe->customers->delete($customer->id, []);
+                        }
+
+                        echo 'error';
+
+                        exit;
+
+                    } catch (\Stripe\Exception\ApiConnectionException $e) {
+
+                        if($customer){
+                            $stripe->customers->delete($customer->id, []);
+                        }
+
+                        echo 'error';
+
+                        exit;
+
+                    } catch (\Stripe\Exception\ApiErrorException $e) {
+
+                        if($customer){
+                            $stripe->customers->delete($customer->id, []);
+                        }
+
+                        echo 'error';
+
+                        exit;
+
+                    } catch (Exception $e) {
+
+                        if($customer){
+                            $stripe->customers->delete($customer->id, []);
+                        }
+
+                        echo 'error';
+
+                        exit;
+
+                    }
+
+
+                    $headers = [];
+                    $headers[] = 'MIME-Version: 1.0';
+                    $headers[] = 'Content-type: text/html; charset=utf-8';
+                    $headers[] = 'From: siterapide.ca <nepasrepondre@siterapide.ca>';
+
+
+                    $to = 'info@siterapide.ca';
+
+                    $subject = 'Nouveau client';
+
+                    $body = '
+                        <html>
+                            <head>
+                                <title>'. $subject .'</title>
+                            </head>
+                            <body>
+                                <p>Bonjour,</p>
+                                <p style="margin-top: 8px;">'. $contactFirstname . ' ' . $contactLastname .' a été ajouté comme client Stripe et utilisateur WordPress.</p>
+                            </body>
+                        </html>
+                    ';
+
+                    wp_mail($to, $subject, $body, implode("\r\n", $headers));
+                }
+            }
+
+            http_response_code(200);
+            echo 'passed';
+
+        },
+        'permission_callback' => '__return_true'
+    ]);
+
     /*register_rest_route('siterapide/v1', 'invoice-upcoming', [
         'methods' => 'POST',
         'callback' => function(){
